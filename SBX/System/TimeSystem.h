0ab694fd146db20a947e9ed8da6fe3bbd25c7328
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-06-21 10:59:20 +0200

    time rate console title

--- SBX/System/TimeSystem.h
+++ SBX/System/TimeSystem.h
@@ -13,21 +13,21 @@ public:
 
 	void Tick( double dt ) override
 	{
-		m_Components->Time.m_Time += dt;
-		m_ConsoleTitle = "Time = " + std::to_string( m_Components->Time.m_Time );
+		m_Components->Time.m_Time += dt * m_Components->Time.m_TimeRate;
+		m_ConsoleTitle = "Time = " + std::to_string( m_Components->Time.m_Time ) + ", TimeRate = " + std::to_string( m_Components->Time.m_TimeRate );
 		SetConsoleTitle( m_ConsoleTitle.c_str() );
 	}
 
 	void StartTime()
 	{
 		m_Components->Time.m_TimeRate = 1;
-		LOG_INFO( "Time started" );
+		LOG_INFO( "Time started at {}", m_Components->Time.m_Time );
 	}
 
 	void StopTime()
 	{
 		m_Components->Time.m_TimeRate = 0;
-		LOG_INFO( "Time stopped" );
+		LOG_INFO( "Time stopped at {}", m_Components->Time.m_Time );
 	}
 
 	void SetTimeRate( double timeRate )
