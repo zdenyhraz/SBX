e78b53f69221de049d3fac49a2e2297e7ad3c1c1
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-07-08 22:15:01 +0200

    health

--- SBX/System/Logic/HealthSubSystem.cpp
+++ SBX/System/Logic/HealthSubSystem.cpp
@@ -8,5 +8,13 @@ HealthSubSystem::HealthSubSystem( std::shared_ptr<ComponentVectors> components, 
 
 void HealthSubSystem::Tick()
 {
+	for ( auto &entity : m_Components->EntityInfos.GetContainer() )
+	{
+		int id = entity.first;
 
+		if ( entity.second.Health <= 0 )
+		{
+			m_Managers->m_EntityManager->DeleteEntity( id );
+		}
+	}
 }
