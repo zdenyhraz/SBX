5ce9727d58408e5beaf92e952157176c4a9717fd
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-07-09 14:45:52 +0200

    parse shader

--- SBX/System/Systems/RenderSystem.cpp
+++ SBX/System/Systems/RenderSystem.cpp
@@ -9,6 +9,58 @@ RenderSystem::RenderSystem( std::shared_ptr<ComponentVectors> components, std::s
 
 }
 
+std::string RenderSystem::ParseShader( const std::string &path )
+{
+	std::ifstream stream( path );
+	std::stringstream ss;
+	std::string line;
+	while ( getline( stream, line ) )
+	{
+		ss << line << "\n";
+	}
+	return ss.str();
+}
+
+unsigned int RenderSystem::CompileShader( unsigned int type, const std::string &source )
+{
+	unsigned int id = glCreateShader( type );
+	const char *src = source.c_str();
+	glShaderSource( id, 1, &src, nullptr );
+	glCompileShader( id );
+
+	int result;
+	glGetShaderiv( id, GL_COMPILE_STATUS, &result );
+	if ( result == GL_FALSE )
+	{
+		int length;
+		glGetShaderiv( id, GL_INFO_LOG_LENGTH, &length );
+		char *message = ( char * )alloca( length * sizeof( char ) );
+		glGetShaderInfoLog( id, length, &length, message );
+		LOG_ERROR( "Failed to compile shader! (type {}) - {}", type, message );
+		glDeleteShader( id );
+		return 0;
+	}
+
+	return id;
+}
+
+unsigned int RenderSystem::CreateShader( const std::string &vertexShader, const std::string &fragmentShader )
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader( GL_VERTEX_SHADER, vertexShader );
+	unsigned int fs = CompileShader( GL_FRAGMENT_SHADER, fragmentShader );
+
+	glAttachShader( program, vs );
+	glAttachShader( program, fs );
+	glLinkProgram( program );
+	glValidateProgram( program );
+
+	glDeleteShader( vs );
+	glDeleteShader( fs );
+
+	return program;
+}
+
 void RenderSystem::Tick()
 {
 	glfwInit();
@@ -20,8 +72,8 @@ void RenderSystem::Tick()
 	float positions[6] =
 	{
 		-0.5f, -0.5f,
-		0.0f,  0.5f,
-		0.5f, -0.5f
+		0.3f,  0.5f,
+		0.5f, -0.7f
 	};
 
 	unsigned int bufferId;
@@ -35,6 +87,17 @@ void RenderSystem::Tick()
 	glEnableVertexAttribArray( attribIdx );
 	glVertexAttribPointer( attribIdx, attribCnt, GL_FLOAT, GL_FALSE, stride, attribPtr );
 
+	std::string vertexShader = ParseShader( "Resources/Shaders/Vertex.shader" );
+	std::string fragmentShader = ParseShader( "Resources/Shaders/Fragment.shader" );
+
+	unsigned int shader = CreateShader( vertexShader, fragmentShader );
+	glUseProgram( shader );
+
+	// there can be other shaders (like compute shader, geometry shader, idk...), but most important are:
+	// 1) vertex shader is run for each vertex (can for example project vertices)
+	// 2) fragment (pixel) shader is run for each pixel that needs to be rasterized (can for example determines color, lighting)
+
+
 	while ( !glfwWindowShouldClose( m_Window ) )
 	{
 		glClear( GL_COLOR_BUFFER_BIT );
@@ -46,6 +109,6 @@ void RenderSystem::Tick()
 		glfwSwapBuffers( m_Window );
 		glfwPollEvents();
 	}
-
+	glDeleteProgram( shader );
 	glfwTerminate();
 }
